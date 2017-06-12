dcb62b7cd664fe7e88550212b972c2b6d70c1ebd
Michael Wong <michaelchunkitwong@gmail.com>
2017-06-11 21:06:54 -0400

    Add some bootstrap

--- index.php
+++ index.php
@@ -2,49 +2,43 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>File API</title>
+	<title>Budgeter</title>
 
-	<link rel="stylesheet" href="css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="includes/chartjs/Chart.js"></script>
-	<script src="includes/generateGraphs.js"></script>
+	<link rel="stylesheet" href="includes/css/bootstrap.min.css">
+	<script src="includes/js/jquery.min.js"></script>
+	<script src="includes/js/bootstrap.min.js"></script>
+	<script src="includes/js/chartjs/Chart.js"></script>
+	<script src="includes/js/generateGraphs.js"></script>
+	<script src="includes/js/moment.js"></script>
 </head>
 <body>
-	<div id="page-wrapper">
-
-		<h1>Text File Reader</h1>
-		<div>
-			Select a text file: 
+	<div id="container">
+		<div class="col-md-10">
+			<h1>Michael's Budgeter</h1>
+			<h3>Select a csv file:</h3>
 			<input type="file" id="fileInput">
-			<button type="button" id="submit" onclick="submit()">Submit</button>
-		</div>
-
-		<br>
-		<pre id="fileDisplayArea"></pre>
-				<div id="canvas-holder">
-			<canvas id="chart-area" width="500" height="500"/>
+			<div class="row">
+				<div class="col-md-12">
+					<input id="new_filter" class="input"></input>
+					<select>
+						<option>Food</option>
+					</select>
+					<button class="btn">Create</button>
+				</div>
+			</div>
+			<div class="row">
+				<div id="canvas-holder" class="col-md-4">
+					<canvas id="chart-area"/>
+				</div>
+				<div class="col-md-6">
+					<pre id="fileDisplayArea"></pre>
+				</div>
+			</div>
 		</div>
 
 	</div>
 	<?php require 'includes/parseFileSetup.php'; ?>
-	<script src="includes/parsefile.js"></script>
-	<script>
-		function submit() {
-			// AJAX code to submit form.
-			console.log(description);
-			$.ajax({
-					type: "POST",
-					url: "includes/submit.php",
-					data: {fileContents: fileContents},
-					cache: false,
-					success: function(html) {
-					alert('Successfully added.');
-				}
-			});
-		
-			return false;
-		}
-	</script>
+	<script src="includes/js/parsefile.js"></script>
 
 </body>
 </html>
